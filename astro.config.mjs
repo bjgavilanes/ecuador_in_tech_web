45ff5b47344a128ee8a39f81c818182d584e5566
Brayen Gavilanes <bjgavilanes@gmail.com>
2024-12-04 13:19:18 -0300

    ci: biome check --write

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwind from '@astrojs/tailwind';
+import tailwind from "@astrojs/tailwind";
 
-import alpinejs from '@astrojs/alpinejs';
+import alpinejs from "@astrojs/alpinejs";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://ecuadorintech.org",
-  integrations: [tailwind(), alpinejs()]
+	site: "https://ecuadorintech.org",
+	integrations: [tailwind(), alpinejs()],
 });
